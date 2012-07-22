609a3ee0393d9abd4b3c132e11b9786b9e9ce3f5
Calvin Froedge <calvinfroedge@gmail.com>
2012-07-21 22:06:47 -0500

    Fixed centering / scaling, added callbacks and hooks system

--- miniModal.plugin.js
+++ miniModal.plugin.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
 
 			this.callbacks = {};
 
+			this.hooks = {};
+
 			var that = this;
 			
 			//Public methods
@@ -18,15 +20,34 @@ $(document).ready(function(){
 				constructor: Modal,
 
 				launch: function(getUrl, callbacks){
+					
+					if(that.hooks.beforeLaunch) that.doHooks(that.hooks.beforeLaunch);
+
 					that.callbacks = callbacks;
 					that.url = getUrl;
 
 					that.getData(that, "load");
+
+					if(that.hooks.afterLaunch) that.doHooks(that.hooks.afterLaunch);
 				},
 
 				refresh: function()
 				{
+					if(that.hooks.beforeRefresh) that.doHooks(that.hooks.beforeRefresh);
+
 					that.getData(that, "reload");
+
+					if(that.hooks.afterRefresh) that.doHooks(that.hooks.afterRefresh);
+				},
+
+				addHook: function(hook, action){
+					if(!that.hooks[hook]) that.hooks[hook] = [];
+
+					that.hooks[hook].push(action);
+				},
+
+				getHooks: function(hook){
+					return that.hooks[hook];
 				}
 			}
 		}
@@ -36,32 +57,50 @@ $(document).ready(function(){
 		{
 			if(!that.url) return false;
 
+			if(that.hooks.beforeGetData) that.doHooks(that.hooks.beforeGetData);
+
 			$.ajax(
 				{
 					url: that.url,
 					success: function(data){
-						if(modalCallback == 'load'){
+						if(modalCallback == 'load')
+						{
 							that.load(that, data);		
 						}
-						else {
+						else 
+						{
 							that.reload(that, data);
 						}
-
 						that.doCallbacks(that);
 					}
 				}
 			);
+
+			if(that.hooks.afterGetData) that.doHooks(that.hooks.afterGetData);
 		}
 
 		Modal.prototype.doCallbacks = function(that)
 		{
+			if(that.hooks.beforeCallbacks) that.doHooks(that.hooks.beforeCallbacks);
+
 			$.each(that.callbacks, function(k, v){
 				that.callbacks[k]();
+			});
+
+			if(that.hooks.beforeCallbacks) that.doHooks(that.hooks.afterCallbacks);
+		}
+
+		Modal.prototype.doHooks = function(hooks)
+		{
+			$.each(hooks, function(k, v){
+				hooks[k]();
 			});
 		}
 
 		Modal.prototype.load = function(that, data)
 		{
+			if(that.hooks.beforeLoad) that.doHooks(that.hooks.beforeLoad);
+
 			that.overlay = $('<div id="overlay"></div>');
 			that.modal = $('<div id="mini_modal"></div>');
 			that.closer = $('<a href="#" class="close">Close</a>');
@@ -73,19 +112,30 @@ $(document).ready(function(){
 			});
 
 			$(that.closer).click(function(){
-				that.close();
+				that.close(that);
 			});
 
-			$(that.modal).prependTo($('body')).center();
-			$('<div class="content">'+data+'</div>').appendTo($(that.modal));
+			$(that.modal).prependTo($('body'));
+			$('<div class="container"><div class="content">'+data+'</div></div>').appendTo($(that.modal));
+			setTimeout(function(){ that.center($(that.modal));}, '100');
+
+			if(that.hooks.beforeLoad) that.doHooks(that.hooks.afterLoad);
 		}
 
 		Modal.prototype.reload = function (that, data)
 		{
+			if(that.hooks.beforeReload) that.doHooks(that.hooks.beforeReload);
+
+			$('#mini_modal .content').children().remove();
+			$(data).appendTo('#mini_modal .content');
+
+			if(that.hooks.beforeReload) that.doHooks(that.hooks.beforeReload);
 		}
 
 		Modal.prototype.close = function(that)
 		{
+			if(that.hooks.beforeClose) that.doHooks(that.hooks.beforeClose);
+
 			var time = '300';
 			$(that.overlay).fadeOut(time);
 			$(that.modal).fadeOut(time);
@@ -93,10 +143,18 @@ $(document).ready(function(){
 				function(){
 					$(that.overlay).remove();
 					$(that.modal).remove();
+					
+					if(that.hooks.afterClose) that.doHooks(that.hooks.afterClose);
 				},
 				time
 			);
 		}
 
+		Modal.prototype.center = function (modal)
+		{
+	    	modal.css("position","absolute");
+   	 		modal.css("top", Math.max(0, (($(window).height() - modal.outerHeight()) / 2)) + "px");
+    		modal.css("left", Math.max(0, (($(window).width() - modal.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+		}
 	})( jQuery );
 });
